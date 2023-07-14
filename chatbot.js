d4cf9bfac7d38acf2ea306a547d68a70eb61efb6
aravindcz <aravindcnair24@gmail.com>
2023-07-14 08:21:57 +0530

    feat: Add the skeleton

---
+++ chatbot.js
@@ -0,0 +1,56 @@
+// Get chatbot elements
+const chatbot = document.getElementById('chatbot');
+const conversation = document.getElementById('conversation');
+const inputForm = document.getElementById('input-form');
+const inputField = document.getElementById('input-field');
+
+// Add event listener to input form
+inputForm.addEventListener('submit', function(event) {
+  // Prevent form submission
+  event.preventDefault();
+
+  // Get user input
+  const input = inputField.value;
+
+  // Clear input field
+  inputField.value = '';
+  const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" });
+
+  // Add user input to conversation
+  let message = document.createElement('div');
+  message.classList.add('chatbot-message', 'user-message');
+  message.innerHTML = `<p class="chatbot-text" sentTime="${currentTime}">${input}</p>`;
+  conversation.appendChild(message);
+
+  // Generate chatbot response
+  const response = generateResponse(input);
+
+  // Add chatbot response to conversation
+  message = document.createElement('div');
+  message.classList.add('chatbot-message','chatbot');
+  message.innerHTML = `<p class="chatbot-text" sentTime="${currentTime}">${response}</p>`;
+  conversation.appendChild(message);
+  message.scrollIntoView({behavior: "smooth"});
+});
+
+// Generate chatbot response function
+function generateResponse(input) {
+    
+let data = {}
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/users",false);
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    data = JSON.parse(xhr.response);
+    console.log(data);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
+
+xhr.send();
+    
+return data["response"]
+  }
+  
+  
